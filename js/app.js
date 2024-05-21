657ad90a53611c9875548f753e73c8c6419773f5
345467Y <yinethpao170@gmail.com>
2024-05-20 21:32:44 -0500

    avances proyecto

---
+++ js/app.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  let nombre = document.getElementById("nombre").value;
+  console.log(nombre);
+  document.getElementById("nombre").value = "";
+  let contrasena = document.getElementById("contrasena").value;
+  console.log(contrasena);
+  document.getElementById("contrasena").value = "";
+  let correo = document.getElementById("correo").value;
+  console.log(correo);
+  document.getElementById("correo").value = "";
+  let telefono = document.getElementById("telefono").value;
+  console.log(telefono);
+  document.getElementById("telefono").value = "";
+  let fechaN = document.getElementById("fechaN").value;
+  console.log(fechaN);
+  document.getElementById("fechaN").value = "";
+}
